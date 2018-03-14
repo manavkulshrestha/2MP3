6c7d4a82dd6cb11e6043627b1f0eb66ce65e5ad4
manavkulshrestha <manavkoolz@gmail.com>
2018-03-14 14:17:53 -0400

    Finished meta data stuff. Add album art functionality

--- 2mp3.py
+++ 2mp3.py
@@ -3,12 +3,55 @@ import os
 import json
 import spotipy.util as util
 import time
+import youtube_dl
+from urllib import parse, request
+from re import findall
+import eyed3
 import pprint
 
 pp = pprint.PrettyPrinter(indent=2)
 
 # PATH = os.path.dirname(__file__)
 spotify_token = None
+PATH = os.path.dirname(os.path.abspath(__file__))
+SONGS_PATH = f'{PATH}/songs/'
+PLACEHOLDER_FILE_NAME = 'song'
+PLACEHOLDER_ART_NAME = 'album_art'
+
+mp3 = {
+		'verbose':			True,
+		'fixup': 			'detect_or_warn',
+		'format': 			'bestaudio/best',
+		'postprocessors': [{
+							'key': 'FFmpegExtractAudio',
+							'preferredcodec': 'mp3',
+							'preferredquality': '320',
+		 }],
+		'extractaudio': 	True,
+		'outtmpl': 			f'{SONGS_PATH}{place}{PLACEHOLDER_FILE_NAME}.%(ext)s',#f'{SONGS_PATH}%(title)s.%(ext)s'
+		'noplaylist': 		True,
+	}
+
+def youtube_search(q):
+	query = parse.urlencode({'search_query' : str.strip(q)})
+	req = request.urlopen(f'https://www.youtube.com/results?{query}')
+	sr = findall(r'href=\"\/watch\?v=(.{11})', req.read().decode())
+	return [f'http://www.youtube.com/watch?v={x}' for x in sr]
+
+def download_image(image_url, file_name):
+    request.urlretrieve(image_url, file_name)
+
+def add_meta_data(name, song):
+	os.rename(f'{SONGS_PATH}{name}', f'{SONGS_PATH}{song["artists"][0]} - {song["title"]}{" (Explicit)" if song["explicit"] else ""}.mp3')
+	audiofile = eyed3.load(name)
+	audiofile.tag.artist = song['artists'][0]
+	audiofile.tag.album = song['album']
+	audiofile.tag.album_artist = ', '.join(song['artists'])
+	audiofile.tag.title = song['title']
+	audiofile.tag.track_num = song['track_number']
+	# audiofile.tag.addImage()
+	audiofile.tag.play_count = 0
+	audiofile.tag.save()
 
 def open_json_file(file_name):
 	with open(file_name, 'r') as file:
@@ -31,18 +74,27 @@ def parse_spotify_playlist_url(url):
 	user_end = url.find('/', user_start)
 	return (url[user_start:user_end], url[url.rfind('/')+1:])
 
+def get_spotify_image_url(images, priority_height, priority_width):
+	for image in images:
+		if image['height'] == priority_height and images['width'] == priority_width:
+			return image['url']
+	return images[0]['url']
+
 def get_spotify_playlist_songs(username, playlist_id):
 	songs = []
 	results = sp.user_playlist_tracks(username, playlist_id=playlist_id)
 	if 'external_urls' in results:
 		results = results['tracks']
 	while True:
+		pp.pprint(results)
 		for item in results['items']:
 			song = {
+				'album': item['track']['album']['name'],
+				'image': get_spotify_image_url(item['track']['album']['images'], 300, 300),
 				'artists': [artist['name'] for artist in item['track']['artists']],
-				'duration_ms': item['track']['duration_ms'],
+				# 'duration_ms': item['track']['duration_ms'],
 				'explicit': item['track']['explicit'],
-				'name': item['track']['name'],
+				'title': item['track']['name'],
 				'track_number': item['track']['track_number'],
 			}
 			songs.append(song)
@@ -59,12 +111,25 @@ spotify_token = get_spotify_token(info)
 
 if spotify_token:
 	sp = spotipy.Spotify(auth=spotify_token)
+else:
+	print(f'No token: {spotipy_token}')
 
 url = 'https://open.spotify.com/user/kieron/playlist/5Rrf7mqN8uus2AaQQQNdc1'
 url = 'https://open.spotify.com/user/manavkoolz/playlist/38emfFLnZtfyOocIXX7AYG?si=MrSNWTTVQm-B4kXreOKefA'
 
 playlist = parse_spotify_playlist_url(url)
 playlist = get_spotify_playlist_songs(playlist[0], playlist[1])
+
+int i=1
+
+with youtube_dl.YoutubeDL(mp3) as ytdl:
+	for song in playlist:
+		query = f'{' '.join(song["artists"])} {song["title"]}{" Dirty" if song["explicit"] else ""} HQ'
+		ytdl.download(youtube_search(query)[:1])
+		add_meta_data(f'{PLACEHOLDER_FILE_NAME}.mp3', song)
+		print(f'{i}. Download complete: {song["title"]}')
+		i += 1
+
 pp.pprint(playlist)
 print(len(playlist))
 
--- ytdl_test.py
+++ ytdl_test.py
@@ -1,41 +1,69 @@
 import youtube_dl
 from urllib import parse, request
 from re import findall
+import eyed3
 import pprint
 import os
 
-SONG_PATH = '/songs'
-pp = pprint.PrettyPrinter(indent=2)
+PATH = os.path.dirname(os.path.abspath(__file__))
+SONGS_PATH = f'{PATH}/songs/'
 
-def youtube_search(q):
-	query = parse.urlencode({'search_query' : str.strip(q)})
-	req = request.urlopen(f'https://www.youtube.com/results?{query}')
-	sr = findall(r'href=\"\/watch\?v=(.{11})', req.read().decode())
-	return [f'http://www.youtube.com/watch?v={x}' for x in sr]
 
-song = {
-	'title': 'Here Comes Your Man'
-}
 
-mp3 = {
-		'verbose':			True,
-		'fixup': 			'detect_or_warn',
-		'format': 			'bestaudio/best',
-		'postprocessors': [{
-							'key': 'FFmpegExtractAudio',
-							'preferredcodec': 'mp3',
-							'preferredquality': '320',
-		 }],
-		'extractaudio': 	True,
-		'outtmpl': 			f'{SONG_PATH[1:]}%(title)s.%(ext)s',
-		'noplaylist': 		True,
-	}
+def download_image(image_url, file_name):
+    request.urlretrieve(image_url, file_name)
 
-search_results = youtube_search('Here Comes Your Man Pixies HQ')[:1]
+download_image('https://i.scdn.co/image/93b671372b0b256ff878c9f78d5dcd7f68fe7a20', f'{SONGS_PATH}album_art.jpg')
 
-with youtube_dl.YoutubeDL(mp3) as ytdl:
-	search_results = [ytdl.extract_info(song) for song in search_results]
-os.execute(f'mv {SONG_PATH}/{search_results['title']}.mp3 {SONG_PATH}/{song['title']}.mp3')
-#set artist name, album artist name, title, thumbnail, track number
+def add_meta_data(songname, song):
+	print(f'PATH: "{PATH}"')
+	os.rename(songname, song)
+	audiofile = eyed3.load(song)
+	audiofile.tag.artist = 'Boyce Avenue'
+	# audiofile.tag.album = ""
+	# audiofile.tag.album_artist = "TIntegrity"
+	audiofile.tag.title = "Let Her Go"
+	audiofile.tag.addImage()
+	# audiofile.tag.track_num = 2
+	audiofile.tag.addImage(0x08, f'{SONGS_PATH}album_art.jpg')
+	audiofile.tag.save()
 
-pp.pprint(search_results)
+add_meta_data(f'{SONGS_PATH}Boyce Avenue - Let Her Go.mp3', f'{SONGS_PATH}Boyce Avenue - Let Her Go.mp3')
+os.remove(f'{SONGS_PATH}album_art.jpg')
+
+# add_meta_data(f'{SONG_PATH}test.mp3', f'{SONG_PATH}Boyce Avenue - Let Her Go.mp3')
+
+# pp = pprint.PrettyPrinter(indent=2)
+
+# def youtube_search(q):
+# 	query = parse.urlencode({'search_query' : str.strip(q)})
+# 	req = request.urlopen(f'https://www.youtube.com/results?{query}')
+# 	sr = findall(r'href=\"\/watch\?v=(.{11})', req.read().decode())
+# 	return [f'http://www.youtube.com/watch?v={x}' for x in sr]
+
+# song = {
+# 	'title': 'Here Comes Your Man'
+# }
+
+# mp3 = {
+# 		'verbose':			True,
+# 		'fixup': 			'detect_or_warn',
+# 		'format': 			'bestaudio/best',
+# 		'postprocessors': [{
+# 							'key': 'FFmpegExtractAudio',
+# 							'preferredcodec': 'mp3',
+# 							'preferredquality': '320',
+# 		 }],
+# 		'extractaudio': 	True,
+# 		'outtmpl': 			f'{SONG_PATH}%(title)s.%(ext)s',
+# 		'noplaylist': 		True,
+# 	}
+
+# search_results = youtube_search('Legend Has It Run The Jewels HQ')[:1]
+
+# with youtube_dl.YoutubeDL(mp3) as ytdl:
+# 	search_results = [ytdl.extract_info(song) for song in search_results]
+# # os.execute(f'mv {SONG_PATH}/{search_results['title']}.mp3 {SONG_PATH}/{song['title']}.mp3')
+# #set artist name, album artist name, title, thumbnail, track number
+
+# pp.pprint(search_results)
